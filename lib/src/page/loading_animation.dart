e3690974c4c6bbdd5fa6dfc7a09beab72f43c851
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-04-15 11:35:08 +0200

    Update loading animation text

--- lib/src/page/loading_animation.dart
+++ lib/src/page/loading_animation.dart
@@ -1,19 +1,20 @@
 part of page;
 
-/// Display a common loading animation to sweeten user's time when connecting.
+/// Display a common animation to sweeten an user's time when connecting.
 class LoadingAnimation extends StatelessWidget with _PageTemplateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _displayAppBar(
-        titleSufix: 'connecting...',
+        titleSufix: 'loading...',
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircularProgressIndicator(
-              semanticsLabel: 'Animated circle as a loading indicator.',
+              semanticsLabel:
+                  'Animated rotating circle as a loading indicator.',
             ),
           ],
         ),
