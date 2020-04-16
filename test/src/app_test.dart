d96e82e6d9c9015f8227f2865d85cec63da6692f
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-04-16 13:17:42 +0200

    Improve app module unit test
    What:
    - a little more meaningful comment,
    - check it's auth member runtimeType.

--- test/src/app_test.dart
+++ test/src/app_test.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rubbish_calc/src/app.dart';
+import 'package:rubbish_calc/src/auth.dart';
 
 void main() {
-  test('The app should initialize their members and launch methods.', () {
+  test('The app module should initialize their members and launch methods.',
+      () {
     final app = App();
+
     expect(app.auth == null, false);
+    expect(app.auth.runtimeType, Auth);
+
     expect(app.createState() == null, false);
   });
 }
